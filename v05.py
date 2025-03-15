03338ba8bf551ffc3455c31b6c4ee1c37d2a2bd3
Parth Suthar <195526412+rjtbknd@users.noreply.github.com>
2025-03-15 22:48:28 +0530

    v05.py

--- v05.py
+++ v05.py
@@ -25,11 +25,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_validate
 
 # NN model code
-try:
-    import tensorflow as tf
-except ModuleNotFoundError:
-    subprocess.run([sys.executable, "-m", "pip", "install", "--user", "tensorflow"], check=True)
-    import tensorflow as tf
+import tensorflow as tf
 from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 #
 
